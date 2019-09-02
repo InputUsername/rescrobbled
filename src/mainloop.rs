9cbbab77a8548bec4ad122aed10b3bd7d0716e22
Evgeny Badin <treeshateorcs@yahoo.com>
2019-09-02 18:47:02 +0300

    fucked up again

--- src/mainloop.rs
+++ src/mainloop.rs
@@ -13,6 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+use listenbrainz_rust::Listen;
 use mpris::{PlaybackStatus, Player, PlayerFinder};
 use rustfm_scrobble::{Scrobble, Scrobbler};
 
