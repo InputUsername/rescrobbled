d2ff5d66e6ec474302d7c5b2751ee1b47b9d827c
Koen Bolhuis <koen.bolhuis@gmail.com>
2025-06-11 23:30:07 +0200

    Update copyright comment

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-// Copyright (C) 2021 Koen Bolhuis
+// Copyright (C) 2025 Koen Bolhuis
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
